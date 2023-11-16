320d2625a9c41d965fcc9145226918f2fc636ad9
Claudia Ageha(Chelsea) <claudiaageha@gmail.com>
2023-11-16 06:16:06 -0800

    gen_author

--- gen_author.sh
+++ gen_author.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
-
-# see also ".mailmap" for how email addresses and names are deduplicated
+# Change directory to the root of the Git repository
+cd "$(git rev-parse --show-toplevel)"
 
 {
-	cat <<- 'EOH'
-		# This file lists all individuals having contributed content to the repository.
-		# For how it is generated, see `hack/generate-authors.sh`.
-	EOH
-	echo
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+    cat <<'EOH'
+        # This file lists all individuals having contributed content to the repository.
+        # For how it is generated, see `hack/generate-authors.sh`.
+EOH
+    echo
+    git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
 } > AUTHORS
+
