ee9113d3f6c835cfc036f480dfd1a9c4dae8cdb1
Meshack Bwire <b.meshack@outlook.com>
2023-11-17 23:27:16 +0300

    simple shell

--- environment.c
+++ environment.c
@@ -7,27 +7,24 @@
  * @exiti: Exit status pointer (unused)
  * Return: 1 on success, 0 on failure
  */
-
 int unset_env(char **av, char *input, int *exiti)
 {
-int number_tokens;
+    int number_tokens = 0;
 
-number_tokens = 0;
+    (void)input;
+    (void)exiti;
 
-(void)input;
-(void)exiti;
+    while (av[number_tokens] != NULL)
+    {
+        number_tokens++;
+    }
 
-while (av[number_tokens] != NULL)
-{
-number_tokens++;
-}
-
-if (number_tokens == 2)
-unsetenv(av[1]);
-else
-fprintf(stderr, "Try 'setenviroment' key value\n");
-return (1);
-
+    if (number_tokens == 2)
+        unsetenv(av[1]);
+    else
+        fprintf(stderr, "Try 'setenvironment' key value\n");
+    
+    return 1;
 }
 
 /**
@@ -37,29 +34,24 @@ return (1);
  * @exiti: Exit status pointer (unused)
  * Return: 1 on success, 0 on failure
  */
-
 int set_env(char **av, char *input, int *exiti)
 {
-int number_tokens;
+    int number_tokens = 0;
 
-number_tokens = 0;
+    (void)input;
+    (void)exiti;
 
-(void)input;
-(void)exiti;
+    while (av[number_tokens] != NULL)
+    {
+        number_tokens++;
+    }
 
-while (av[number_tokens] != NULL)
-{
-number_tokens++;
-}
+    if (number_tokens == 3)
+        setenv(av[1], av[2], 1);
+    else if (number_tokens < 3)
+        fprintf(stderr, "Try 'setenvironment' key value\n");
+    else
+        fprintf(stderr, "Try 'setenvironment' key value\n");
 
-if (number_tokens == 3)
-setenv(av[1], av[2], 1);
-else if (number_tokens < 3)
-fprintf(stderr, "Try 'setenviroment' key value\n");
-else
-
-fprintf(stderr, "Try  'setenviroment' key value\n");
-
-return (1);
-
+    return 1;
 }
